bfcc803e8a19bd0836536f9e005dd6c32371613a
Teddy Economou <teddyecon@gmail.com>
2023-01-11 20:42:31 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+function updateContent(message) {
+let p = document.addEventListener("DOMContentLoaded", function(event) {
+  const p = document.querySelector('p'); 
+  p.textContent = message;
+  });
+}
+updateContent('This is really cool!')
